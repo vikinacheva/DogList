6390ebdda650c51283398e59fe63f4ef4d538797
vikinacheva <vikinacheva04@gmail.com>
2022-11-11 09:29:58 +0200

    Add project files.

---
+++ DogList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DogList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace DogList.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
---
+++ DogList/Models/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogList.Models
+{
+    public class Dog
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; } 
+    }
+}
